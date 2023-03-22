7bdb7006a34780c07d420cd8af5b1f11b95dd68a
Deepika8383 <117986650+Deepika8383@users.noreply.github.com>
2023-03-22 23:49:09 +0530

    question one of assignment one

---
+++ Q1_A1.java
@@ -0,0 +1,11 @@
+public class Q1_A1{
+    public static void main(String[] args) {
+    System.out.println("*****");
+    System.out.println("   *");
+    System.out.println("  *");
+    System.out.println(" *");
+    System.out.println("*****");
+        
+    }
+        
+    }
